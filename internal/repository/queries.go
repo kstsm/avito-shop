e4e4a50d78ab4eb50cd2673bd255ae9160c2d8ab
kst.sm <63549958+kstsm@users.noreply.github.com>
2025-02-27 21:03:27 +0600

    Удалил неиспользуемый sql запрос

--- internal/repository/queries.go
+++ internal/repository/queries.go
@@ -31,44 +31,23 @@ const (
 		INSERT INTO transactions (from_user, to_user, amount) 
 		VALUES ($1, $2, $3);`
 	
-	QueryTransferCoins = `
-		WITH updated AS (
-    UPDATE users
-    SET balance = CASE
-                    WHEN id = $1 THEN balance - $2 
-                    WHEN id = $3 THEN balance + $2
-                  END
-    WHERE id IN ($1, $3)
-    RETURNING id
-)
-SELECT COUNT(*)
-FROM updated
-WHERE id IN ($1, $3);`
-
 	QueryGetUserInfo = `
-SELECT json_build_object(
-  'coins', u.balance,
-  'inventory', (
-    SELECT COALESCE(json_agg(json_build_object('type', i.name, 'quantity', inv.quantity)), '[]'::json)
-    FROM inventory inv
-    JOIN items i ON inv.item_id = i.id
-    WHERE inv.user_id = u.id
-  ),
-  'coinHistory', json_build_object(
-    'received', (
-      SELECT COALESCE(json_agg(json_build_object('fromUser', u2.username, 'amount', t.amount)), '[]'::json)
-      FROM transactions t
-      JOIN users u2 ON t.from_user = u2.id
-      WHERE t.to_user = u.id
-    ),
-    'sent', (
-      SELECT COALESCE(json_agg(json_build_object('toUser', u2.username, 'amount', t.amount)), '[]'::json)
-      FROM transactions t
-      JOIN users u2 ON t.to_user = u2.id
-      WHERE t.from_user = u.id
-    )
-  )
-) 
-FROM users u 
-WHERE u.id = $1;`
+		SELECT json_build_object(
+  			'coins', u.balance,'inventory', (
+   		SELECT COALESCE(json_agg(json_build_object('type', i.name, 'quantity', inv.quantity)), '[]'::json)
+    		FROM inventory inv
+    		JOIN items i ON inv.item_id = i.id
+    		WHERE inv.user_id = u.id),
+  			'coinHistory', json_build_object(
+    			'received', (
+      		SELECT COALESCE(json_agg(json_build_object('fromUser', u2.username, 'amount', t.amount)), '[]'::json)
+      		FROM transactions t
+      		JOIN users u2 ON t.from_user = u2.id
+      		WHERE t.to_user = u.id),'sent', (
+      	      	SELECT COALESCE(json_agg(json_build_object('toUser', u2.username, 'amount', t.amount)), '[]'::json)
+      	      	FROM transactions t
+      	      	JOIN users u2 ON t.to_user = u2.id
+      	      	WHERE t.from_user = u.id))) 
+		FROM users u 
+		WHERE u.id = $1;`
 )
